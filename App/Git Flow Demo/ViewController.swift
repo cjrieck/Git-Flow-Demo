365032cc77b0045f8a042045e1e853385e72b001
Clayton Rieck <cjrieck123@gmail.com>
2014-11-21 16:07:44 -0500

    added label with awesome new text

--- App/Git Flow Demo/ViewController.swift
+++ App/Git Flow Demo/ViewController.swift
@@ -14,6 +14,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.view.backgroundColor = UIColor.whiteColor()
+        
+        var textLabel = UILabel(frame: CGRectMake(0, 0, self.view.frame.size.width, 50))
+        textLabel.center = self.view.center
+        textLabel.textAlignment = .Center
+        textLabel.font = UIFont.systemFontOfSize(30.0)
+        textLabel.text = "GitHub Flow Demo!"
+        self.view.addSubview(textLabel)
     }
 
     override func didReceiveMemoryWarning() {
